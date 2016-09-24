d0336f4262575c1450b08723cf21a696fe763fa0
nqdinh <nqdinhddt@gmail.com>
2016-09-24 19:03:51 +0700

    simple proxy

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict'
 
 let http = require('http')
+let request = require('request')
+let destinationUrl = '127.0.0.1:8000'
 
 http.createServer((req, res) => {
     for (let header in req.headers) {
@@ -8,3 +10,14 @@ http.createServer((req, res) => {
     }
     req.pipe(res)
 }).listen(8000)
+
+http.createServer((req, res) => {
+    console.log(`Proxying request to: ${destinationUrl + req.url}`)
+    // Proxy code here
+    let options = {
+        headers: req.headers,
+        url: `http://${destinationUrl}${req.url}`,
+        method: req.method
+    }
+    req.pipe(request(options)).pipe(res)
+}).listen(8001)
