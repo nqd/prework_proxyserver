1b010ee401289678647b80723f6fa7cf40941e23
nqdinh <dinh.nguyen@veriksystems.com>
2016-09-26 21:50:19 +0700

    get and post test for echo func

--- test/request.js
+++ test/request.js
@@ -1,17 +1,29 @@
 'use strict'
 
 let server = require('../index.js')
-let supertest = require('supertest')
+// let supertest = require('supertest')
 let assert = require('assert')
+let request = require('request')
 
 describe('Proxy server', () => {
-    it('should perform echo service', (done) => {
-        supertest(server)
-        .get('/')
-        .expect(200, (err, res) => {
+    it('should perform echo service with get', (done) => {
+        request('http://localhost:8001/abc', (err, res, body) => {
             assert.ifError(err)
-            console.log(res.body)
+            assert.equal(res.statusCode, 200)
+            assert.equal(body, '')
             done()
         })
+    })
+    it('should perform echo service with post', (done) => {
+        // with form: Content-type: application/x-www-form-urlencoded
+        request.post({url:'http://localhost:8001/abc',
+            form: {key:'value'}},
+            (err, res, body) => {
+                assert.ifError(err)
+                assert.equal(res.statusCode, 200)
+                assert.equal(body, 'key=value')
+                done()
+            }
+        )
     })
 })
