0f567122948cb2126b82dd5400f383e9b669ad2a
nqdinh <dinh.nguyen@veriksystems.com>
2016-10-01 22:05:38 +0700

    add test cases that fire https request to port 8002

--- test/request.js
+++ test/request.js
@@ -26,4 +26,24 @@ describe('Proxy server', () => {
             }
         )
     })
+    it('should perform https echo service with get', (done) => {
+        request('https://localhost:8002/abc', (err, res, body) => {
+            assert.ifError(err)
+            assert.equal(res.statusCode, 200)
+            assert.equal(body, '')
+            done()
+        })
+    })
+    it('should perform https echo service with post', (done) => {
+        // with form: Content-type: application/x-www-form-urlencoded
+        request.post({url:'https://localhost:8002/abc',
+            form: {key:'value'}},
+            (err, res, body) => {
+                assert.ifError(err)
+                assert.equal(res.statusCode, 200)
+                assert.equal(body, 'key=value')
+                done()
+            }
+        )
+    })
 })
